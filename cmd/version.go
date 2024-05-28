6b64c5c54ad308549570ea58dfdda4e963692cb9
imingz <imingz@qq.com>
2024-05-28 23:20:26 +0800

    improvement: 给 version 添加短描述

--- cmd/version.go
+++ cmd/version.go
@@ -13,7 +13,8 @@ var GitVersion = "" // gitVersion 是语义化的版本号.
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
-	Use: "version",
+	Use:   "version",
+	Short: "Print the version of voacap",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(GitVersion)
 	},
